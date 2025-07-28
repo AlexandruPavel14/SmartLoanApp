c616818ed57fc0859dcbbe9da70d18584c8bd85a
Alexandru Pavel <alexandru14pavel@gmail.com>
2025-07-28 23:56:56 +0300

    Initial miscellaneous code for controllers

---
+++ Step3-Application-implementation/backend/SmartLoanAppAPI/src/main/java/com/alexandrupavel/smartloanappapi/controller/misc/HelloController.java
@@ -0,0 +1,13 @@
+package com.alexandrupavel.smartloanappapi.controller.misc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/v1/hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
